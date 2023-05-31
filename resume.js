f06315bf65083bf0f717a359af40a043564ea60d
vetapalem <krishnavetapalem2019@gmail.com>
2023-05-31 16:05:54 +0530

    trail_resume

---
+++ resume.js
@@ -0,0 +1,9 @@
+// const toggleButton = document.getElementById('dark-mode-toggle');
+
+// toggleButton.addEventListener('click', function() {
+//   document.body.classList.toggle('dark-mode');
+// });
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
